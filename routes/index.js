7f42de5ac9141527df1c1899ba676834eac325b8
Ofem Eteng <ofemeteng@gmail.com>
2017-08-02 19:44:14 +0100

    update app.js index.js

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,15 @@
 var express = require('express')
 var router = express.Router()
+var getIP = require('ipware')().get_ip
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	[ipaddress, language, software] = getHeaderParams(req)
-	console.log(ipaddress)
 	createJson(res, ipaddress, language, software)
 })
 
 function getHeaderParams(req) {
-	var ipaddress = req.ip.split(':')[3]
+	var ipaddress = getIP(req).clientIp.split(':')[3] || req.ip.split(':')[3] || req.connection.remoteAddress.split(':')[3]
 	var language = req.headers["accept-language"].split(',')[0]
 	var software = req.headers['user-agent']
 	var start = software.indexOf('(')
