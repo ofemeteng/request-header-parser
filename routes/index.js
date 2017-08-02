9a35a7fe3f8df20b0e34ba7df5490a1592d1b577
Ofem Eteng <ofemeteng@gmail.com>
2017-08-02 19:37:34 +0100

    update app.js index.js

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ var router = express.Router()
 /* GET home page. */
 router.get('/', function(req, res, next) {
 	[ipaddress, language, software] = getHeaderParams(req)
+	console.log(ipaddress)
 	createJson(res, ipaddress, language, software)
 })
 
