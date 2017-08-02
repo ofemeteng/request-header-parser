5aee3a5771c077463f396f32ee2b705770f8c8fb
Ofem Eteng <ofemeteng@gmail.com>
2017-08-02 14:50:05 +0100

    update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,36 @@
 var express = require('express')
+var request = require('request')
 var router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-	var ipaddress = req.ip.split(':')[3]
-	// var ipaddress = req.connection.remoteAddress.split(':')[3]
-	console.log(req.headers)
+	var ipaddress = ''
+	request({
+		uri: 'http://freegeoip.net/json',
+		method: 'GET'
+	}, (error, response, body) => {
+		if (!error && response.statusCode == 200) {
+			let json = JSON.parse(body)
+			var ipaddress = json.ip
+			var [language, software] = getHeaderParams(req)
+			createJson(res, ipaddress, language, software)
+		} else {
+			var ipaddress = req.ip.split(':')[3]
+			var [language, software] = getHeaderParams(req)
+			createJson(res, ipaddress, language, software)
+			console.error('Offline: ', error);
+		}
+	})
+});
+
+function getHeaderParams(req) {
 	var language = req.headers["accept-language"].split(',')[0]
 	var software = req.headers['user-agent']
 	var start = software.indexOf('(')
 	var end = software.indexOf(')')
 	software = software.slice(start + 1, end)
-
-	console.log(req.headers['user-agent'])
-  createJson(res, ipaddress, language, software)
-});
+	return [language, software]
+}
 
 function createJson(res, ipaddress, language, software) {
 	return res.format({
